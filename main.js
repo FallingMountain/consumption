a12d23724671b8d0332a64cb70cd8c958e913898
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-05-24 15:11:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ var game = {
 		logarithm: {
 			base: 10
 		}
-	}
-  ;
+	},
+	money:new Decimal(0);
+};
+
+setInterval(function(){
+document.getElementById("atoms").innerHTML = game.money
+
+	    }, 10)
